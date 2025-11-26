6a4c4f468d67ae87f7fb493553a9f6691ea62d90
mummidisatyasantosh <mummidisatyasantosh@gmail.com>
2025-11-26 15:21:24 +0530

    Add files via upload

---
+++ c-11.c
@@ -0,0 +1,10 @@
+//write a C program to find maximum of three numbers using conditionally operator 
+#include<stdio.h>
+int main()
+{
+   int a,b,c,max;
+   printf("Enter any three integer numbers");
+   scanf("%d%d%d",&a,&b,&c);
+   max=a > b ? (a > c ? a : c) :( b > c ? b : c);
+   printf("maximum of three numbers %d %d %d is %d",a,b,c,max);
+}
